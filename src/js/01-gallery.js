79a9ad6bdf9af8532862b8e20ab45b3334ff6ed3
ImbaMaksym <137620059+ImbaMaksym@users.noreply.github.com>
2023-11-21 20:32:29 +0200

    Hello world

---
+++ src/js/01-gallery.js
@@ -0,0 +1,9 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
