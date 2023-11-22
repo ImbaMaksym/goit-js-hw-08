b2e6227468c065e33bb4ed5d3857432bac6d932d
ImbaMaksym <137620059+ImbaMaksym@users.noreply.github.com>
2023-11-22 21:08:05 +0200

    final

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,17 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
 
-const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-const updateLocalStorage = throttle(function (data) {
-  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data.seconds));
-}, 1000);
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
 
-player.on('timeupdate', updateLocalStorage);
-getCurrentTime();
-
-function getCurrentTime() {
-  const currentTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-  player.setCurrentTime(currentTime);
-}
+player.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time'))
+);
